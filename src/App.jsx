ab606936a8999bc91596f9e53bea5b23ee8a06bd
okky <okkypratama.dev@gmail.com>
2025-01-18 18:03:07 +0700

    add create employee form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import EmployeeComponent from './components/EmployeeComponent'
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
       <HeaderComponent />
-      <ListEmployeeComponent />
+        <Routes>
+          {/* http://localhost:3000 */}
+          <Route path='/' element= { <ListEmployeeComponent /> }></Route>
+          {/* http://localhost:3000/employees */}
+          <Route path='/employees' element= { <ListEmployeeComponent /> }></Route>
+          {/* http://localhost:3000/add-employee */}
+          <Route path='/add-employee' element= { <EmployeeComponent /> }></Route>
+        </Routes>
       <FooterComponent />
+    </BrowserRouter>
     </>
   )
 }
